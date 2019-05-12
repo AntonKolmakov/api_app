537f86fe68b4a30040e27e9d7ba07ce4179674e3
Anton Kolmakov <kolmakoff@icloud.com>
2019-05-12 15:35:14 +0300

    fix get_ips request

--- app/interactors/get_ips.rb
+++ app/interactors/get_ips.rb
@@ -2,9 +2,25 @@ class GetIps
   include Interactor
 
   def call
-    context.result = Post
-                         .select('posts.ip', 'users.login')
-                         .joins(:user)
-                         .distinct(:ip)
+    find_ips
+    context.result = find_duplicates(find_ips)
+  end
+
+  protected
+
+  def find_ips
+    arr = Post.select('posts.ip', 'users.login').joins(:user).map { |c| [c.ip.to_s, c.login] }
+  end
+
+  def find_duplicates(arr)
+    ips = {}
+    arr.each do |element|
+      if ips.has_key?(element[0])
+        ips[element[0]] << ",#{element[1]}"
+      else
+        ips[element[0]] = element[1]
+      end
+    end
+    ips
   end
 end
--- spec/requests/get_ips_request_spec.rb
+++ spec/requests/get_ips_request_spec.rb
@@ -5,12 +5,16 @@ RSpec.describe 'Rate API', type: :request do
   describe 'GET /get_ips' do
 
     context 'user get rate posts' do
-      let!(:posts) { create_pair(:post)}
+      let!(:posts1) { create(:post, ip: '192.168.2.0', user: create(:user, login: 'login_1'))}
+      let!(:posts2) { create(:post, ip: '192.168.2.0', user: create(:user, login: 'login_2'))}
+      let!(:posts3) { create(:post, ip: '180.110.5.0', user: create(:user, login: 'login_3'))}
 
       before { get '/rates/get_ips' }
 
       it 'should return ip and login users' do
         expect(json).not_to be_empty
+        expect(json['192.168.2.0']).to eq('login_1,login_2')
+        expect(json['180.110.5.0']).to eq('login_3')
       end
 
       it 'returns status code 200' do
